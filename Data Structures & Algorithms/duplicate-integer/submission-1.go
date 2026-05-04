4e8ff1ddde3c92fda02df681bdac8e2178ed19a5
Ray Law <raycar@gmail.com>
2026-05-04 21:43:54 +0100

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.go
@@ -0,0 +1,10 @@
+func hasDuplicate(nums []int) bool {
+    seen := make(map[int]struct{}, len(nums))
+	for _, v := range nums {
+		if _, ok := seen[v]; ok {
+			return true
+		}
+		seen[v] = struct{}{}
+	}
+	return false
+}
