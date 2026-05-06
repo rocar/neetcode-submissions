e7bdc575660f84faf3d2ed13ceb8cd8ce22252b5
Ray Law <raycar@gmail.com>
2026-05-06 23:01:01 +0100

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.go
@@ -0,0 +1,22 @@
+func topKFrequent(nums []int, k int) []int {
+    count := make(map[int]int)
+    for _, num := range nums {
+        count[num]++
+    }
+
+    freq := make([][]int, len(nums)+1)
+    for num, c := range count {
+        freq[c] = append(freq[c], num)
+    }
+
+    res := []int{}
+    for i := len(freq)-1; i > 0; i-- {
+        for _, num := range freq[i] {
+            res = append(res, num)
+            if len(res) == k {
+                return res
+            }
+        }
+    }
+    return res
+}
