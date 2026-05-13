0c7dcd383aa92968e08936961a0573b2a9e601ec
Ray Law <raycar@gmail.com>
2026-05-13 19:01:01 +0100

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.go
@@ -0,0 +1,31 @@
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	found := [][]int{}
+	
+	for i := 0; i < len(nums); i++ {
+
+		if i > 0 && nums[i] == nums[i-1]{
+			continue
+		}
+		
+		l, r := i + 1, len(nums) - 1
+		for l < r {
+			sum := nums[i] + nums[l] + nums[r]
+			switch {
+				case sum == 0:
+					found = append(found, []int{nums[i], nums[l], nums[r]})
+					l++
+					r--
+
+					for l < r && nums[l] == nums[l-1] {
+						l++
+					}
+				case sum > 0:
+					r--
+				default:
+					l++
+			}
+		}
+	}
+	return found
+}
